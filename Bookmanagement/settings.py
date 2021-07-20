a38fce2a8bf9629990837e6e18c7d2ef4feddd8f
Samir Maharjan <samir84753@gmail.com>
2021-07-20 21:09:28 +0545

    added env files

--- Bookmanagement/settings.py
+++ Bookmanagement/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&++1-al4i17=&ggc!j$sse8fi=r4x6#v#q7xyvmvmc4b6rm*p='
+SECRET_KEY = os.getenv('key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
